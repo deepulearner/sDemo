edef1b0d21ee2b66e3a2a05b62935e6a7a175dba
deepulearner <deepulearner@gmail.com>
2022-03-02 22:59:59 +0530

    sDemo Login Study

---
+++ Test Suites/ts_1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>be757273-4935-4817-8b00-f7c9f77fec06</testSuiteGuid>
+   <testCaseLink>
+      <guid>10a9fc40-0f35-44ad-bb43-c16533cd1aaa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
